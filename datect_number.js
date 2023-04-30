f09b308eb4e8b8a884d70cb268dc091a97c4abf6
MD Zunaeed Imran <124905281+Zunaeed-Imran@users.noreply.github.com>
2023-04-30 14:19:06 +0600

    Number type show in javaScript

---
+++ datect_number.js
@@ -0,0 +1,9 @@
+let num = prompt("Enter any number: ");
+
+if (num > 0) {
+  document.write("Number is Posetive");
+} else if (num < 0) {
+  document.write("Number is Negative");
+} else {
+  document.write("Number is Zero");
+}
